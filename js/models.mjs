7f59641b5773e1ae2172f87beb0b694fb13c71e0
Justin Foley <justin.foley@oup.com>
2024-07-06 14:41:23 +0100

    Triangles structure built

--- js/models.mjs
+++ js/models.mjs
@@ -19,26 +19,61 @@ export class Yantra {
     var fem1 = new FeminineTriangle(this.circleRadius, centreX, centreY);
     fem1.draw(two);
 
+    var masc1 = new MasculineTriangle(this.circleRadius, centreX, centreY);
+    masc1.draw(two);
+
     var fem2 = new FeminineTriangle(this.circleRadius*3/4, centreX, centreY-50);
     fem2.draw(two);
 
   }
 }
 
-class FeminineTriangle {
+class Triangle {
+  constructor(points) {
+    this.points = points;
+  }
+
+  draw(two) {
+    var top = two.makeLine(this.points[0][0], this.points[0][1], this.points[1][0], this.points[1][1]);
+    var left = two.makeLine(this.points[0][0], this.points[0][1], this.points[2][0], this.points[2][1]);
+    var right = two.makeLine(this.points[1][0], this.points[1][1], this.points[2][0], this.points[2][1]);
+  }
+}
+
+class FeminineTriangle extends Triangle {
   constructor(sideLength, centreX, centreY) {
+    let left= [centreX - sideLength, centreY];
+    let right= [centreX + sideLength, centreY]
+    let bottom = [centreX, centreY + sideLength]
+    let points = [left, right, bottom];
+
+    super(points);
+
     this.sideLength = sideLength;
     this.centreX = centreX;
     this.centreY = centreY;
 
-    this.left= [this.centreX - this.sideLength, this.centreY];
-    this.right= [this.centreX + this.sideLength, this.centreY]
-    this.bottom = [this.centreX, this.centreY + this.sideLength]
+    this.left = left;
+    this.right = right;
+    this.bottom = bottom;
   }
+}
 
-    draw(two) {
-      var top = two.makeLine(this.left[0], this.left[1], this.right[0], this.right[1]);
-      var left = two.makeLine(this.left[0], this.left[1], this.bottom[0], this.bottom[1]);
-      var right = two.makeLine(this.right[0], this.right[1], this.bottom[0], this.bottom[1]);
-    }
+class MasculineTriangle extends Triangle {
+  constructor(sideLength, centreX, centreY) {
+    let left= [centreX - sideLength, centreY];
+    let right= [centreX + sideLength, centreY]
+    let top = [centreX, centreY - sideLength]
+    let points = [left, right, top];
+
+    super(points);
+
+    this.sideLength = sideLength;
+    this.centreX = centreX;
+    this.centreY = centreY;
+
+    this.left = left;
+    this.right = right;
+    this.top = top;
+  }
 }
